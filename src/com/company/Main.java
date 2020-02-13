ba4eda1bcd912d4edbfbad41d1dcd5cf605fe4d6
SlintPunk <artur.matuszkiewicz@gmail.com>
2020-02-13 20:36:45 +0100

    Finish Phantom Reference trening

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,53 +3,59 @@ package com.company;
 import java.lang.ref.PhantomReference;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        //Create ReferenceQueue for Map references
         ReferenceQueue<Map> referenceQueue = new ReferenceQueue<>();
+        //Create MapFinalizer that is PhantomReference
         List<MapFinalizer> mapFinalizerMap = new ArrayList<>();
+        //Lists of maps
         List<Map> mapList = new ArrayList<>();
 
+        //started creating new maps and populate array
         for (int i = 0 ; i < 20 ; i++){
-            Map map = new HashMap(){
-                {
-                    put("Test1", "Test2");
-                    put("Test3", "Test4");
-                }
-            };
+            //create new map with random values
+            Map map = new HashMap();
+            for (int x = 0 ; x < 20 ; x++) {
+                map.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+            }
+            //add new map to list of map
             mapList.add(map);
+            //add reference to spy for given map - create new MapFinalizer(PhantomReference) with queue
             mapFinalizerMap.add(new MapFinalizer(map, referenceQueue));
         }
 
-
-        //Object is not enqueue (strong references)
+        //Object is not enqueue (still strong references)
         //All finalizers has referent
-        mapFinalizerMap
-                .forEach(x -> System.out.println("In GC Queue? " + x.isEnqueued()));
+        displayIfObjectAreGcEnqued((List<MapFinalizer>) mapFinalizerMap);
 
-
-
-
-        System.out.println("Remove map list");
+        //ReferenceQueue is empty for now
+        System.out.println("ReferenceQueue is empty? " + (referenceQueue.poll() == null));
+        //Remove map list strong reference
         mapList = null;
+        //need to to "suggest" gc to start work - just suggestion :)
         System.gc();
 
         //Object is in queue
         //All finalizers don't have any referent
-        mapFinalizerMap
-                .forEach(x -> System.out.println("In GC Queue? " + x.isEnqueued()));
+        displayIfObjectAreGcEnqued(mapFinalizerMap);
 
-
+        //Right now after GC cleans, all references should be visible in referenceQueue
         Reference<?> reference;
         while((reference = referenceQueue.poll()) != null){
-            System.out.println("In GC QUEUE: " + reference.isEnqueued());
+            //remove from queue
+            reference.clear();
         }
+
+        //So in case of normal application - we can use PhantomReference to be sure that some "big" object is removed from heap before we want to take some other
+    }
+
+    private static void displayIfObjectAreGcEnqued(List<MapFinalizer> mapFinalizerMap) {
+        mapFinalizerMap
+                .forEach(x -> System.out.println("In GC Queue? " + x.isEnqueued()));
     }
 }
 
